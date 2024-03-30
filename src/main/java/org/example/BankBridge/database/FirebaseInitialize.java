936a329fe99b3ac27ea95182c2551899dd46e4f0
Tasnim Ferdous <tferdous611@gmail.com>
2024-03-29 22:11:21 -0400

    create init class for Firebase connection with initialize() method

---
+++ src/main/java/org/example/BankBridge/database/FirebaseInitialize.java
@@ -0,0 +1,23 @@
+package org.example.BankBridge.database;
+
+import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+
+public class FirebaseInitialize {
+    public static void initialize() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/org/example/BankBridge/key.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
