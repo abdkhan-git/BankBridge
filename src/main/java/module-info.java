8bf6a256f3c23a3e9f1a6a55fb2eec9fef01b7a1
Tasnim Ferdous <tferdous611@gmail.com>
2024-03-29 22:10:41 -0400

    add firebase.admin and com.google.auth.oauth2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example.BankBridge {
     requires javafx.controls;
     requires javafx.fxml;
+    requires firebase.admin;
+    requires com.google.auth.oauth2;
 
 
     opens org.example.BankBridge to javafx.fxml;
