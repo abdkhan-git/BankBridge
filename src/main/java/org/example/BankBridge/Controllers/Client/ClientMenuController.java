4ff1f3d119579dc8fd7123feba0f506f7a9d7190
DarnItDannn <danish.syed1219@gmail.com>
2024-04-02 21:19:38 -0400

    Added ClientMenu.css Styles to ClientMenu.fxml

--- src/main/java/org/example/BankBridge/Controllers/Client/ClientMenuController.java
+++ src/main/java/org/example/BankBridge/Controllers/Client/ClientMenuController.java
@@ -1,4 +1,23 @@
 package org.example.BankBridge.Controllers.Client;
 
-public class ClientMenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboardBtn;
+    public Button transactionBtn;
+    public Button accountsBtn;
+    public Button profileBtn;
+    public Button logoutBtn;
+    public Button reportBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }
+
 }
