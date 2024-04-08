b05516c1575236c9e8f5ba61e6dca3272c937b3c
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-07 21:17:43 -0400

    implement registerUser functionality that takes in User obj

--- src/main/java/org/example/BankBridge/Database/service/FirebaseService.java
+++ src/main/java/org/example/BankBridge/Database/service/FirebaseService.java
@@ -1,4 +1,38 @@
 package org.example.BankBridge.Database.service;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import org.example.BankBridge.App;
+import org.example.BankBridge.Database.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Contains necessary database (CRUD) operations
+ */
 public class FirebaseService {
+
+    public boolean registerUser(User user) {
+        // Create user request with only email/phone num for authentication
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setEmail(user.email())
+                .setPhoneNumber(user.phoneNum())
+                .setDisabled(false);
+
+        UserRecord record;
+        try {
+            record = App.fauth.createUser(request);
+            addNewUserToDb(user, record.getUid());
+            return true;
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
