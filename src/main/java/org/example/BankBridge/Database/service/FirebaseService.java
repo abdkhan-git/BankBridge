d6ab7aba23e9a4bfb17a1f33f4d917a0f49f5c18
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-07 21:56:09 -0400

    implement method to retrieve all registered users from the DB

--- src/main/java/org/example/BankBridge/Database/service/FirebaseService.java
+++ src/main/java/org/example/BankBridge/Database/service/FirebaseService.java
@@ -2,6 +2,8 @@ package org.example.BankBridge.Database.service;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
@@ -9,9 +11,8 @@ import org.example.BankBridge.App;
 import org.example.BankBridge.Database.model.Account;
 import org.example.BankBridge.Database.model.User;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Contains necessary database (CRUD) operations
@@ -65,6 +66,35 @@ public class FirebaseService {
 
         //asynchronously write data
         ApiFuture<WriteResult> result = docRef.set(accountData);
+    }
+
+    public List<User> retrieveAllRegisteredUsers() {
+        List<User> listOfRegisteredUsers = new ArrayList<>();
+
+        ApiFuture<QuerySnapshot> future =  App.fstore.collection("users").get();
+        List<QueryDocumentSnapshot> documents;
+        try {
+            documents = future.get().getDocuments();
+            if(!documents.isEmpty()) {
+                documents.stream()
+                        .map(document -> new User(
+                                String.valueOf(document.get("first_name")),
+                                String.valueOf(document.get("last_name")),
+                                String.valueOf(document.get("email")),
+                                String.valueOf(document.get("phone_num")),
+                                String.valueOf(document.get("password")),
+                                String.valueOf(document.get("account_type"))
+                        ))
+                        .forEach(listOfRegisteredUsers::add);
+            }
+            else {
+                System.out.println("No registered users found within database.");
+            }
+        }
+        catch (InterruptedException | ExecutionException ex) {
+            System.out.println("Error occurred. Possibly due to AGE not being captured during registration.");
+        }
+        return listOfRegisteredUsers;
     }
 
 
