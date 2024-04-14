b60b582832369775fa09bef0d9dcff85413a71ea
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-14 11:08:28 -0400

    implement login method functionality to verify email + password

--- src/main/java/org/example/BankBridge/Controllers/LoginController.java
+++ src/main/java/org/example/BankBridge/Controllers/LoginController.java
@@ -1,13 +1,18 @@
 package org.example.BankBridge.Controllers;
 
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.example.BankBridge.App;
+import org.example.BankBridge.Database.service.FirebaseService;
 import org.example.BankBridge.Models.Model;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,6 +25,8 @@ public class LoginController implements Initializable {
     public Button login_btn;
     public Label error_lbl;
 
+    private final FirebaseService firebaseService = new FirebaseService();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         login_btn.setOnAction(event -> onLogin());
@@ -29,5 +36,26 @@ public class LoginController implements Initializable {
         Stage stage = (Stage) error_lbl.getScene().getWindow();
         Model.getInstance().getViewFactory().closeStage(stage);
         Model.getInstance().getViewFactory().showClientWindow();
+    }
+
+    // TODO: Utilize login() method in event handler method for login button when ready
+    public void login() {
+        String email = user_address_lbl.getText();
+        String password = password_fld.getText();
+
+        try {
+            UserRecord user = App.fauth.getUserByEmail(email);
+            if (user != null) {
+                String uuid = user.getUid();
+                if (password.equals(firebaseService.retrievePersonByUuidAndReturnPass(uuid))) {
+                    // switch screens
+                } else {
+                    System.out.println("ERROR: Invalid password.");
+                }
+            }
+        } catch (FirebaseAuthException e) {
+            System.out.println("ERROR: Could not sign in.");
+        }
+
     }
 }
