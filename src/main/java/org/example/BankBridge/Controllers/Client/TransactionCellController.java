044ae346ddecae6f0d9b536305500f30c07115a2
abhiskeks31 <shara55@farmingdale.edu>
2024-04-21 01:07:21 -0400

    Added Transaction Cell and it's CSS file

---
+++ src/main/java/org/example/BankBridge/Controllers/Client/TransactionCellController.java
@@ -0,0 +1,23 @@
+package org.example.BankBridge.Controllers.Client;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransactionCellController implements Initializable {
+
+    public FontAwesomeIconView in_icon;
+    public FontAwesomeIconView out_icon;
+    public Label trans_date_lbl;
+    public Label sender_lbl;
+    public Label receiver_lbl;
+    public Label amount_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
