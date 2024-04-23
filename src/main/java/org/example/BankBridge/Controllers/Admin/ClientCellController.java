7725c1db820a74a3cdda9446eeccc09460a3440c
DarnItDannn <danish.syed1219@gmail.com>
2024-04-23 19:30:21 -0400

    ClientCell FXML, Controller, and CSS Created

---
+++ src/main/java/org/example/BankBridge/Controllers/Admin/ClientCellController.java
@@ -0,0 +1,24 @@
+package org.example.BankBridge.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+
+    public Label fName_lbl;
+    public Label lName_lbl;
+    public Label pAddress_lbl;
+    public Label ch_acc_lbl;
+    public Label sv_acc_lbl;
+    public Label date_lbl;
+    public Button delete_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
