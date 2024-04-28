de46d6b0e6056416b6427689adac6f58a37050bf
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-28 12:00:02 -0400

    implement deposit money functionality

--- src/main/java/org/example/BankBridge/Controllers/Admin/DepositController.java
+++ src/main/java/org/example/BankBridge/Controllers/Admin/DepositController.java
@@ -1,9 +1,13 @@
 package org.example.BankBridge.Controllers.Admin;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import org.example.BankBridge.App;
+import org.example.BankBridge.Database.model.User;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +20,12 @@ public class DepositController implements Initializable {
     public TextField amount_fld;
     public Button deposit_btn;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
+
+    @FXML
+    public void depositBtnOnClick(ActionEvent event) {
+        App.firebaseService.updateAccountWithNewBalance(pAddress_fld.getText(), Double.parseDouble(amount_fld.getText()));
+    }
 }
