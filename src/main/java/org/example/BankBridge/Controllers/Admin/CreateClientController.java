6368bcab4125e9218a48b86aaeb3afeb8f78c075
abhiskeks31 <shara55@farmingdale.edu>
2024-04-29 12:15:31 -0400

    added logout button

--- src/main/java/org/example/BankBridge/Controllers/Admin/CreateClientController.java
+++ src/main/java/org/example/BankBridge/Controllers/Admin/CreateClientController.java
@@ -1,11 +1,18 @@
 package org.example.BankBridge.Controllers.Admin;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.example.BankBridge.Models.Model;
+import org.example.BankBridge.Views.AccountType;
 
-public class CreateClientController {
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class CreateClientController implements Initializable {
     public TextField fName_fld;
     public TextField lName_fld;
     public TextField password_fld;
@@ -17,4 +24,23 @@ public class CreateClientController {
     public TextField sv_amount_fld;
     public Label error_lbl;
     public Button createclient_btn;
+
+    private String payeeAddress;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private void createCheckingAccount(){
+        double balance = Double.parseDouble(ch_amount_fld.getText());
+        //Generate Account Number
+
+        String firstSection = "3201";
+        String lastSection = Integer.toString((new Random()).nextInt(9999)+1000);
+        String accountNumber = firstSection + " " + lastSection;
+        //Create the Checking number
+        // creating this later
+    // Model.getInstance().getDatabaseDriver.createCheckingAccount(payeeAddress, accountNumber, /* Transaction Limit*/10, balance);
+    }
 }
