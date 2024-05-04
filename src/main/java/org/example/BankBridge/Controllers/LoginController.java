7954f053ee0d1397a8152721265916316046aec0
Tasnim Ferdous <tferdous611@gmail.com>
2024-05-04 14:22:07 -0400

    refactor: finish onLoginBtnClick() and change method names

--- src/main/java/org/example/BankBridge/Controllers/LoginController.java
+++ src/main/java/org/example/BankBridge/Controllers/LoginController.java
@@ -3,6 +3,7 @@ package org.example.BankBridge.Controllers;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -31,21 +32,10 @@ public class LoginController implements Initializable {
         acc_selector.setItems(FXCollections.observableArrayList(AccountType.CLIENT, AccountType.ADMIN));
         acc_selector.setValue(Model.getInstance().getViewFactory().getLoginAccountType());
         acc_selector.valueProperty().addListener(observable -> Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue()));
-        login_btn.setOnAction(event -> onLogin());
     }
 
-    private void onLogin(){
-        Stage stage = (Stage) error_lbl.getScene().getWindow();
-        Model.getInstance().getViewFactory().closeStage(stage);
-       if(Model.getInstance().getViewFactory().getLoginAccountType()==AccountType.CLIENT){
-           Model.getInstance().getViewFactory().showClientWindow();
-       }
-       else{
-           Model.getInstance().getViewFactory().showAdminWindow();
-       }
-    }
-
-    public void login() {
+    @FXML
+    public void onLoginBtnClick() {
         String email = user_address_lbl.getText();
         String password = password_fld.getText();
 
@@ -55,6 +45,7 @@ public class LoginController implements Initializable {
                 String uuid = user.getUid();
                 if (password.equals(App.firebaseService.retrievePersonByUuidAndReturnPass(uuid))) {
                     // switch screens
+                    changeScene();
                 } else {
                     System.out.println("ERROR: Invalid password.");
                 }
@@ -62,6 +53,16 @@ public class LoginController implements Initializable {
         } catch (FirebaseAuthException e) {
             System.out.println("ERROR: Could not sign in.");
         }
+    }
 
+    private void changeScene(){
+        Stage stage = (Stage) error_lbl.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        if(Model.getInstance().getViewFactory().getLoginAccountType()==AccountType.CLIENT){
+            Model.getInstance().getViewFactory().showClientWindow();
+        }
+        else{
+            Model.getInstance().getViewFactory().showAdminWindow();
+        }
     }
 }
