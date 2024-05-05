db0ae3f8de8dca9239daf6e9a9f05470df32006d
Tasnim Ferdous <tferdous611@gmail.com>
2024-05-05 14:49:51 -0400

    refactor: change checkingAccount to checkingAccountRef w/ StringProperty

--- src/main/java/org/example/BankBridge/Models/Client.java
+++ src/main/java/org/example/BankBridge/Models/Client.java
@@ -4,7 +4,6 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import org.example.BankBridge.Database.model.Account;
 
 import java.time.LocalDate;
 
@@ -12,16 +11,16 @@ public class Client {
     private final StringProperty firstName;
     private final StringProperty lastName;
     private final StringProperty userAddress;
-    private final ObjectProperty<Account> checkingAccount;
-    private final ObjectProperty<Account> savingsAccount;
+    private final StringProperty checkingAccountRef;
+    private final StringProperty savingsAccountRef;
     private final ObjectProperty<LocalDate> dateCreated;
 
-    public Client(String fName, String lName, String uAddress, Account cAccount, Account sAccount, LocalDate date) {
+    public Client(String fName, String lName, String uAddress, String checkingAccountRef, String savingAccountRef, LocalDate date) {
         this.firstName = new SimpleStringProperty(this, "FirstName", fName);
         this.lastName = new SimpleStringProperty(this, "LastName", lName);
         this.userAddress = new SimpleStringProperty(this, "User Address", uAddress);
-        this.checkingAccount = new SimpleObjectProperty<>(this, "Checking Account", cAccount);
-        this.savingsAccount = new SimpleObjectProperty<>(this, "Savings Account", sAccount);
+        this.checkingAccountRef = new SimpleStringProperty(this, "Checking Account", checkingAccountRef);
+        this.savingsAccountRef = new SimpleStringProperty(this, "Savings Account", savingAccountRef);
         this.dateCreated = new SimpleObjectProperty<>(this, "Date Created", date);
     }
 
@@ -36,11 +35,11 @@ public class Client {
     public StringProperty uAddressProperty() {
         return userAddress;
     }
-    public ObjectProperty<Account> checkingAccountProperty() {
-        return checkingAccount;
+    public StringProperty checkingAccountProperty() {
+        return checkingAccountRef;
     }
-    public ObjectProperty<Account> savingsAccountProperty() {
-        return savingsAccount;
+    public StringProperty savingsAccountProperty() {
+        return savingsAccountRef;
     }
 
     public ObjectProperty<LocalDate> dateProperty() {
